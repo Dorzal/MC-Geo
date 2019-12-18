82b505b4a07d0c9a9e993ccdcb20d54bb999ab51
Linder <vhelyno@gmail.com>
2019-12-18 16:27:23 +0100

    WIP : api/location

---
+++ app/src/Controller/AgenceController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AgenceController extends AbstractController
+{
+
+    private $_em;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->_em = $entityManager;
+    }
+
+    /**
+     * @Route("/api/localisation", methods={"POST"})
+     */
+    public function localiser(Request $request) {
+
+        $truc = $request->getContent();
+
+        return new Response($truc);
+    }
+
+}
